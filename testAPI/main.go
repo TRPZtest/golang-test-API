babb90ba8f3acd0dec73d5a3b1fc8b433a12277d
Dima <vishnevsky.dmtr@gmail.com>
2023-03-15 21:49:03 +0200

    fix main

--- testAPI/main.go
+++ testAPI/main.go
@@ -1,35 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	handlers "testAPI/handlers"
-	"testAPI/models"
 
 	"github.com/gorilla/mux"
 )
-
-func POST(w http.ResponseWriter, r *http.Request) {
-
-	var requestBody models.GetMaxPriceRequest
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-	}
-
-	json.Unmarshal(reqBody, &requestBody)
-
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func getMaxPrice(w http.ResponseWriter, r *http.Request) {
-
-}
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
